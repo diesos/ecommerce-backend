a77857e8af5da7ca7ffc022f63790004fbaad9cc
Diesos <ozturk@live.Fr>
2024-08-09 23:50:33 +0200

    Unit testing databases

---
+++ src/test/ressources/data.sql
@@ -0,0 +1,5 @@
+-- Encrypted using : https://www.javainuse.com/onlineBcrypt
+
+INSERT INTO local_user (email, email_verified, first_name, last_name, password, username)
+    VALUES ('UserA@junit.com', true, 'UserA-FirstName', 'UserA-LastName', '$2a$10$4wizppaY1iwKYk3J0Ys7DOEHbmzt7KuXvyiHivTi4GV/eUXGrIsqa', 'UserA') ,
+         ('UserB@junit.com', false, 'UserB-FirstName', 'UserB-LastName', '$2a$10$4wizppaY1iwKYk3J0Ys7DOEHbmzt7KuXvyiHivTi4GV/eUXGrIsqa', 'UserB')
